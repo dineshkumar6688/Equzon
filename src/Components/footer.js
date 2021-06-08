1685d8e326d1ed5a5a88db70e0760e8a5b95fe19
dineshkumar6688 <58526233+dineshkumar6688@users.noreply.github.com>
2021-06-08 14:12:29 +0530

    Update footer.js

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'font-awesome/css/font-awesome.css'
 import '../Style/footer.css';
-import { Container, Col, Row, Image } from 'reactstrap'
+import { Col, Row } from 'react-bootstrap'
 
 function Footer() {
     return (
